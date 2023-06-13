04426ff7ea2c46b48e30177a2328343c63255a67
Aleksey <1aleksey.mazur@gmail.com>
2023-06-13 21:42:28 +0300

    essential functionality hooked up (checking game status; check winner or tie)

--- vanilla/js/app.js
+++ vanilla/js/app.js
@@ -12,6 +12,42 @@ const App = {
     moves: [],
   },
 
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter(move => move.playerId === 1)
+      .map(move => +move.squareId);
+    const p2Moves = moves
+      .filter(move => move.playerId === 2)
+      .map(move => +move.squareId);
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach(pattern => {
+      const p1Wins = pattern.every(v => p1Moves.includes(v));
+      const p2Wins = pattern.every(v => p2Moves.includes(v));
+
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status:
+        moves.length === 9 || winner !== null ? 'complete' : 'in-progress', // in-progress | complete
+      winner, // 1| 2 | null
+    };
+  },
+
   init() {
     App.registerEventListeners();
   },
@@ -32,7 +68,14 @@ const App = {
     App.$.squares.forEach(square => {
       square.addEventListener('click', event => {
         // Check if there is already a play, if so, return early
-        if (square.hasChildNodes()) {
+        const hasMove = squareId => {
+          const existingMove = App.state.moves.find(
+            move => move.squareId === squareId
+          );
+          return existingMove !== undefined;
+        };
+
+        if (hasMove(+square.id)) {
           return;
         }
 
@@ -57,21 +100,18 @@ const App = {
           playerId: currentPlayer,
         });
 
-        App.state.currentPlayer = currentPlayer === 1 ? 2 : 1;
-
         square.replaceChildren(icon);
 
         // Check if there is a winner or tie game
-        const winningPatterns = [
-          [1, 2, 3],
-          [1, 5, 9],
-          [1, 4, 7],
-          [2, 5, 8],
-          [3, 5, 7],
-          [3, 6, 9],
-          [4, 5, 6],
-          [7, 8, 9],
-        ];
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === 'complete') {
+          if (game.winner) {
+            alert(`Player ${game.winner} wins!`);
+          } else {
+            alert('Tie!');
+          }
+        }
       });
     });
   },
