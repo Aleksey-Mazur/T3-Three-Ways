8c79e86a31f01e8ee99854c4020545209807f359
Aleksey <1aleksey.mazur@gmail.com>
2023-07-30 04:05:32 +0300

    Local Storage integration: initView function created in app.js; implemented loading current game with state from local storage after game loading with view.initilizeMoves

--- vanilla/js/app.js
+++ vanilla/js/app.js
@@ -20,11 +20,8 @@ function init() {
   const view = new View();
   const store = new Store('live-t3-storage-key', players);
 
-  view.bindGameResetEvent(event => {
+  function initView() {
     view.closeAll();
-
-    store.reset();
-
     view.clearMoves();
     view.setTurnIndicator(store.game.currentPlayer);
     view.updateScoreboard(
@@ -32,18 +29,19 @@ function init() {
       store.stats.playerWithStats[1].wins,
       store.stats.ties
     );
+    view.initilizeMoves(store.game.moves);
+  }
+
+  initView();
+
+  view.bindGameResetEvent(event => {
+    store.reset();
+    initView();
   });
 
   view.bindNewRoundEvent(event => {
     store.newRound();
-    view.closeAll();
-    view.clearMoves();
-    view.setTurnIndicator(store.game.currentPlayer);
-    view.updateScoreboard(
-      store.stats.playerWithStats[0].wins,
-      store.stats.playerWithStats[1].wins,
-      store.stats.ties
-    );
+    initView();
   });
 
   view.bindPlayerMoveEvent(square => {
