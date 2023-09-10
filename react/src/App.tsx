d61e30b55466adfe9afc7fd1b52c4eb10754322c
Aleksey <1aleksey.mazur@gmail.com>
2023-09-10 20:32:16 +0300

    refactoring to react app: helper methods moved from App.tsx to utils.ts; switch turn indicator logic implemented

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,78 +5,7 @@ import Menu from './components/Menu';
 import Modal from './components/Modal';
 import { GameState, Player } from './types';
 import classNames from 'classnames';
-
-const players: Player[] = [
-  {
-    id: 1,
-    name: 'Player 1',
-    iconClass: 'fa-x',
-    colorClass: 'turquoise',
-  },
-  {
-    id: 2,
-    name: 'Player 2',
-    iconClass: 'fa-o',
-    colorClass: 'yellow',
-  },
-];
-
-function deriveGame(state: GameState) {
-  const currentPlayer = players[state.currentGameMoves.length % 2];
-
-  const winningPatterns = [
-    [1, 2, 3],
-    [1, 5, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 5, 7],
-    [3, 6, 9],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
-
-  let winner = null;
-
-  for (const player of players) {
-    const selectedSquareIds = state.currentGameMoves
-      .filter(move => move.player.id === player.id)
-      .map(move => move.squareId);
-
-    for (const pattern of winningPatterns) {
-      if (pattern.every(v => selectedSquareIds.includes(v))) {
-        winner = player;
-      }
-    }
-  }
-
-  return {
-    moves: state.currentGameMoves,
-    currentPlayer,
-    status: {
-      isComplete: winner !== null || state.currentGameMoves.length === 9,
-      winner,
-    },
-  };
-}
-
-function deriveStats(state: GameState) {
-  return {
-    playerWithStats: players.map(player => {
-      const wins = state.history.currentRoundGames.filter(
-        game => game.status.winner?.id === player.id
-      ).length;
-
-      return {
-        ...player,
-        wins,
-      };
-    }),
-
-    ties: state.history.currentRoundGames.filter(
-      game => game.status.winner === null
-    ).length,
-  };
-}
+import { deriveGame, deriveStats } from './utils';
 
 export default function App() {
   const [state, setState] = useState<GameState>({
@@ -131,10 +60,12 @@ export default function App() {
   return (
     <>
       <main>
-        <div className="grid" data-id="grid">
-          <div className="turn" data-id="turn">
-            <i className="fa-solid fa-x turquoise"></i>
-            <p className="turquoise">Player 1, you're up!</p>
+        <div className="grid">
+          <div className={classNames('turn', game.currentPlayer.colorClass)}>
+            <i
+              className={classNames('fa-solid', game.currentPlayer.iconClass)}
+            ></i>
+            <p>{game.currentPlayer.name}, you're up!</p>
           </div>
 
           <Menu onAction={action => resetGame(action === 'new-round')} />
@@ -172,21 +103,21 @@ export default function App() {
             style={{ backgroundColor: 'var(--turquoise)' }}
           >
             <p>Player 1</p>
-            <span data-id="p1-wins">{stats.playerWithStats[0].wins} Wins</span>
+            <span>{stats.playerWithStats[0].wins} Wins</span>
           </div>
           <div
             className="score shadow"
             style={{ backgroundColor: 'var(--light-gray)' }}
           >
             <p>Ties</p>
-            <span data-id="ties">{stats.ties}</span>
+            <span>{stats.ties}</span>
           </div>
           <div
             className="score shadow"
             style={{ backgroundColor: 'var(--yellow)' }}
           >
             <p>Player 2</p>
-            <span data-id="p2-wins">{stats.playerWithStats[1].wins} Wins</span>
+            <span>{stats.playerWithStats[1].wins} Wins</span>
           </div>
         </div>
       </main>
